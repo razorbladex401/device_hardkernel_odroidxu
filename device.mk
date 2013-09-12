2de7be6a752302be4c883b8a4d431075c6ab7707
razorbladex401 <razorbladex401@gmail.com>
2013-09-12 16:32:14 -0400

    Modifications for building under CM10.2

--- device.mk
+++ device.mk
@@ -78,22 +78,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	gps.odroidxu
 
-# Custom Update packages
-PRODUCT_PACKAGES += \
-    OdroidUpdater
-
 # OpenMAX IL configuration files
 PRODUCT_COPY_FILES += \
 	device/hardkernel/odroidxu/media_profiles.xml:system/etc/media_profiles.xml \
 	device/hardkernel/odroidxu/media_codecs.xml:system/etc/media_codecs.xml
-
-# Input device calibration files
-PRODUCT_COPY_FILES += \
-	device/hardkernel/proprietary/bin/odroid-ts.idc:system/usr/idc/odroidxu-ts.idc \
-	device/hardkernel/proprietary/bin/odroid-ts.kl:system/usr/keylayout/odroidxu-ts.kl \
-	device/hardkernel/proprietary/bin/odroid-ts.kcm:system/usr/keylayout/odroidxu-ts.kcm \
-	device/hardkernel/proprietary/bin/odroid-keypad.kl:system/usr/keylayout/odroidxu-keypad.kl \
-	device/hardkernel/proprietary/bin/odroid-keypad.kcm:system/usr/keychars/odroidxu-keypad.kcm
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
@@ -111,50 +99,19 @@ PRODUCT_COPY_FILES += \
 
 
 #
-# USB Ethernet Module
+# Modules
 #
 PRODUCT_COPY_FILES += \
-	device/hardkernel/proprietary/bin/smsc95xx.ko:system/lib/modules/smsc95xx.ko
-
-#
-# WIFI firmware & Module
-#
-PRODUCT_COPY_FILES += \
-	device/hardkernel/proprietary/bin/rtl8192cu.ko:system/lib/modules/rtl8192cu.ko
+	device/hardkernel/odroidxu/modules/smsc95xx.ko:system/lib/modules/smsc95xx.ko \
+	device/hardkernel/odroidxu/modules/rtl8192cu.ko:system/lib/modules/rtl8192cu.ko
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	wifi.interface=wlan0 \
 	wifi.supplicant_scan_interval=15
-	
-# XBox 360 Controller kl keymaps
-PRODUCT_COPY_FILES += \
-	device/hardkernel/proprietary/bin/Vendor_045e_Product_0291.kl:system/usr/keylayout/Vendor_045e_Product_0291.kl \
-	device/hardkernel/proprietary/bin/Vendor_045e_Product_0719.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl \
-	device/hardkernel/proprietary/bin/Vendor_045e_Product_0719.kcm:system/usr/keychars/Vendor_045e_Product_0719.kcm
-
-PRODUCT_COPY_FILES += \
-	device/hardkernel/proprietary/apk/Ultra_Explorer.apk:system/app/Ultra_Explorer.apk \
-	device/hardkernel/proprietary/apk/jackpal.androidterm.apk:system/app/jackpal.androidterm.apk \
-	device/hardkernel/proprietary/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
-
-PRODUCT_COPY_FILES += \
-	device/hardkernel/proprietary/apk/DicePlayer.apk:system/app/DicePlayer.apk \
-	device/hardkernel/proprietary/lib/libSoundTouch.so:system/lib/libSoundTouch.so \
-	device/hardkernel/proprietary/lib/libdice_jb.so:system/lib/libdice_jb.so \
-	device/hardkernel/proprietary/lib/libdice_jb2.so:system/lib/libdice_jb2.so \
-	device/hardkernel/proprietary/lib/libdice_loadlibrary.so:system/lib/libdice_loadlibrary.so \
-	device/hardkernel/proprietary/lib/libdice_software.so:system/lib/libdice_software.so \
-	device/hardkernel/proprietary/lib/libdice_software_jb.so:system/lib/libdice_software_jb.so \
-	device/hardkernel/proprietary/lib/libffmpeg_dice.so:system/lib/libffmpeg_dice.so \
-	device/hardkernel/proprietary/lib/libsonic.so:system/lib/libsonic.so
-	
+		
 ## Not the prettiest hack ever, but it'll do until JPlayer is understood
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/../proprietary/etc/98netflix:system/etc/init.d/98netflix
-
-# init.d support
-PRODUCT_COPY_FILES += \
-	device/hardkernel/proprietary/bin/sysinit:system/bin/sysinit
+    device/hardkernel/odroidxu/conf/98netflix:system/etc/init.d/98netflix
 
 PRODUCT_PROPERTY_OVERRIDES := \
 	ro.opengles.version=131072 \
@@ -165,8 +122,6 @@ PRODUCT_PROPERTY_OVERRIDES := \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mass_storage
 
-# WQXGA_LCD
-ifeq ($(BOARD_USES_WQXGA_LCD),true)
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
 
@@ -177,21 +132,9 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
-else
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
 
-PRODUCT_CHARACTERISTICS := phone
-
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.heapgrowthlimit=128m
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-endif
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 $(call inherit-product-if-exists, hardware/samsung_slsi/exynos5/exynos5.mk)
-$(call inherit-product-if-exists, vendor/samsung_slsi/exynos5410/exynos5410-vendor.mk)
+$(call inherit-product-if-exists, vendor/hardkernel/odroidxu/odroidxu-vendor.mk)
